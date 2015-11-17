2f62d0e1d619e4a17953444571b41e14303b93e3
sunil gupta <sunil.gupta@impetus.co.in>
2015-11-17 17:25:13 +0530

    CreateDatabase controller first cut checked-in

---
+++ EEJ1_Service_Aggregator/src/main/java/com/imp/saas/web/DatabaseController.java
@@ -0,0 +1,36 @@
+package com.imp.saas.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.imp.saas.handler.DatabaseHandler;
+import com.imp.saas.ws.bean.CreateDBResponse;
+import com.imp.saas.ws.bean.InputData;
+
+@Controller
+@RequestMapping(value = "/webservice")
+public class DatabaseController {
+
+	@Autowired
+	DatabaseHandler databaseHandler;
+	
+	@RequestMapping("hello")
+	public String test(){
+		return "Gjhgashdas";
+	}
+
+	@RequestMapping(value = "/createDatabase", method=RequestMethod.POST)
+	@ResponseBody
+	public CreateDBResponse createDatabase(@RequestBody InputData inputData) {
+
+		String result = databaseHandler.createDatabase(inputData.getDbName(),
+				inputData.getDbUserName(), inputData.getDbPassword(),
+				inputData.getDbURL());
+		return new CreateDBResponse(result);
+
+	}
+}
---
+++ EEJ1_Service_Aggregator/src/main/java/com/imp/saas/ws/bean/CreateDBResponse.java
@@ -0,0 +1,19 @@
+package com.imp.saas.ws.bean;
+
+public class CreateDBResponse {
+
+	private String result;
+
+	public CreateDBResponse(String result) {
+		this.result = result;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
