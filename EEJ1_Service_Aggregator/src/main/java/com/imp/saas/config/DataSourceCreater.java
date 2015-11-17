6e8527aff159c9d1cccef48a5b288db318d72068
rakesh.singhania <rakesh.singhania@impetus-N408.impetus.co.in>
2015-11-17 13:59:49 +0530

    Fixed error.

--- EEJ1_Service_Aggregator/src/main/java/com/imp/saas/config/DataSourceCreater.java
+++ EEJ1_Service_Aggregator/src/main/java/com/imp/saas/config/DataSourceCreater.java
@@ -5,17 +5,17 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 
-public class DataSourceCreater {
+public class DataSourceCreater
+{
 
-	public static DataSource getDataSourceFromDataSourceProperties(
-			DataSourceProperties dataSourceProperties) {
-		DataSourceBuilder factory = DataSourceBuilder
-				.create(dataSourceProperties.getClassLoader())
-				.driverClassName(dataSourceProperties.getDriverClassName())
-				.username(dataSourceProperties.getUsername())
-				.password(dataSourceProperties.getPassword())
-				.url(dataSourceProperties.getUrl());
-		return factory.build();
-	}
+  public static DataSource getDataSourceFromDataSourceProperties(
+    DataSourceProperties dataSourceProperties)
+  {
+    DataSourceBuilder factory = DataSourceBuilder.create(dataSourceProperties.getClassLoader())
+      .driverClassName(dataSourceProperties.getDriverClassName())
+      .username(dataSourceProperties.getUsername()).password(dataSourceProperties.getPassword())
+      .url(dataSourceProperties.getUrl());
+    return factory.build();
+  }
 
 }
--- EEJ1_Service_Aggregator/src/main/java/com/imp/saas/config/MultitenancyProperties.java
+++ EEJ1_Service_Aggregator/src/main/java/com/imp/saas/config/MultitenancyProperties.java
@@ -6,77 +6,97 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("spring.multitenancy")
-public class MultitenancyProperties {
+public class MultitenancyProperties
+{
 
-	/*
-	 * @NestedConfigurationProperty private DataSourceProperties datasource1;
-	 * 
-	 * @NestedConfigurationProperty private DataSourceProperties datasource2;
-	 * 
-	 * @NestedConfigurationProperty private DataSourceProperties datasource3;
-	 */
+  Map<String, DataSourceProperties> datasourceMap;
+  private List<String> urls;
+  private List<String> usernames;
+  private List<String> passwords;
+  @Value("spring.multitenancy.driver-class-name")
+  private String driverClassName;
 
-	Map<String, DataSourceProperties> datasourceMap;
+  @PostConstruct
+  public void setDatasourceMap()
+  {
+    datasourceMap = new HashMap<>();
+    for (int index = 0; index < urls.size(); index++)
+    {
+      String url = urls.get(index);
+      String username = usernames.get(index);
+      String password = passwords.get(index);
+      DataSourceProperties dataSourceProperties = new DataSourceProperties();
+      dataSourceProperties.setUrl(url);
+      dataSourceProperties.setUsername(username);
+      dataSourceProperties.setPassword(password);
+      dataSourceProperties.setDriverClassName(driverClassName);
+      String tenant = url.split("=")[1];
+      datasourceMap.put(tenant, dataSourceProperties);
+    }
+  }
+  
+  public List<String> getPassword()
+  {
+    return passwords;
+  }
 
-	private List<String> urls;
-	private List<String> usernames;
+  public void setPassword(List<String> passwords)
+  {
+    this.passwords = passwords;
+  }
 
-	/**
-	 * @return the urls
-	 */
-	public List<String> getUrls() {
-		return urls;
-	}
+  /**
+   * @return the urls
+   */
+  public List<String> getUrls()
+  {
+    return urls;
+  }
 
-	/**
-	 * @return the usernames
-	 */
-	public List<String> getUsernames() {
-		return usernames;
-	}
+  /**
+   * @return the usernames
+   */
+  public List<String> getUsernames()
+  {
+    return usernames;
+  }
 
-	/**
-	 * @param urls
-	 *            the urls to set
-	 */
-	public void setUrls(List<String> urls) {
-		this.urls = urls;
-	}
+  /**
+   * @param urls the urls to set
+   */
+  public void setUrls(List<String> urls)
+  {
+    this.urls = urls;
+  }
 
-	/**
-	 * @param usernames
-	 *            the usernames to set
-	 */
-	public void setUsernames(List<String> usernames) {
-		this.usernames = usernames;
-	}
+  /**
+   * @param usernames the usernames to set
+   */
+  public void setUsernames(List<String> usernames)
+  {
+    this.usernames = usernames;
+  }
 
-	/**
-	 * @return the datasourceMap
-	 */
-	public Map<String, DataSourceProperties> getDatasourceMap() {
-		return datasourceMap;
-	}
+  /**
+   * @return the datasourceMap
+   */
+  public Map<String, DataSourceProperties> getDatasourceMap()
+  {
+    return datasourceMap;
+  }
 
-	@PostConstruct
-	public void setDatasourceMap() {
-		datasourceMap = new HashMap<>();
-		for (int i = 0; i < urls.size(); i++) {
-			String url = urls.get(i);
-			String username = usernames.get(i);
-			DataSourceProperties dataSourceProperties = new DataSourceProperties();
-			dataSourceProperties.setUrl(url);
-			dataSourceProperties.setUsername(username);
-			dataSourceProperties.setPassword("impetus@123");
-			dataSourceProperties
-					.setDriverClassName("net.sourceforge.jtds.jdbc.Driver");
-			String tenent = url.split("=")[1];
-			datasourceMap.put(tenent, dataSourceProperties);
-		}
-	}
+  public String getDriverClassName()
+  {
+    return driverClassName;
+  }
 
+  public void setDriverClassName(String driverClassName)
+  {
+    this.driverClassName = driverClassName;
+  }
 }
