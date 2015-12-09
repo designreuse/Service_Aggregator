2fed0b0577e73da17d3abdd0d33551f197a761ae
sunil gupta <sunil.gupta@impetus.co.in>
2015-12-09 22:48:56 +0530

    Corrected ProviderController.java

--- EEJ1_Service_Aggregator/src/main/java/com/imp/saas/web/ProviderController.java
+++ EEJ1_Service_Aggregator/src/main/java/com/imp/saas/web/ProviderController.java
@@ -4,11 +4,12 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.imp.saas.domain.Provider;
 import com.imp.saas.repository.ProviderRepository;
 
 /**
@@ -19,25 +20,22 @@ import com.imp.saas.repository.ProviderRepository;
  */
 @Controller
 @RequestMapping("/{tenantid}")
-public class ProviderController
-{
+public class ProviderController {
 
-  @Autowired
-  private ProviderRepository providerRepository;
+	@Autowired
+	private ProviderRepository providerRepository;
 
-  @RequestMapping(value = "/addProvider", method = RequestMethod.POST)
-  @Transactional
-  public String addEmployee(@ModelAttribute
-  Provider provider)
-  {
-    try
-    {
-      providerRepository.save(provider);
-    }
-    catch (Exception e)
-    {
-      return "redirect:/databaseException";
-    }
-    return "redirect:/{tenantid}";
-  }
+	@RequestMapping(value = "/addProvider", method = { RequestMethod.GET,
+			RequestMethod.POST })
+	@Transactional
+	@ResponseBody
+	public String addProvider(@PathVariable String tenantid, Model model) {
+		try {
+			model.addAttribute("tenantid", tenantid);
+			// providerRepository.save(provider);
+		} catch (Exception e) {
+			return "Provider not inserted in database";
+		}
+		return "Provider successfully added in the database";
+	}
 }
